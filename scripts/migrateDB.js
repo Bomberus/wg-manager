1d034cae070c1e86660b0d0e07dd7668fc61b8e4
Pascal Maximilian Bremer <mail@pmbremer.de>
2018-02-25 12:57:17 +0100

    Migrate fix

--- scripts/migrateDB.js
+++ scripts/migrateDB.js
@@ -8,11 +8,21 @@ const r = require('rethinkdbdash')({
 function setup () {
     return new Promise(async ( resolve, reject ) => {
         try {
+            const dbExists = await r.dbList().contains("test").run()
+            if (dbExists)
+                await r.dbDrop("test").run()
             await r.dbCreate("test").run()
             console.log("Creating test db")
+            
+            const tableExists = await r.db("test").tableList().contains("test").run()
+            if (tableExists)
+                await r.db("test").tableDrop("users").run()
             await r.db("test").tableCreate("users").run()
             console.log("Creating test tables")
-            await r.db('rethinkdb').table('users').insert({id: 'test', password: 'test'}).run()
+            await r.db("rethinkdb").table('users').contains("test").do(function(user){
+                return r.db("rethinkdb").table('users').get("test").delete()
+                 }).run()
+            await r.db("rethinkdb").table('users').insert({id: 'test', password: 'test'}).run()
             console.log("Insert Test User")
             await r.db("test").grant('test', {write: true, read: true}).run()
             console.log("Grant Test User access to DB")
